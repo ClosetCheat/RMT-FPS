685b75d1c103818c4e68a5ea6d9516b6020a4f28
MateaLukiccc <ml20200410@student.fon.bg.ac.rs>
2023-02-19 18:26:12 +0100

    bug fixes

--- FPS/Assets/Scripts/Managers/PlayerNameManager.cs
+++ FPS/Assets/Scripts/Managers/PlayerNameManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Photon.Pun;
+using PlayFab;
+using PlayFab.ClientModels;
 
 public class PlayerNameManager : MonoBehaviour
 {
@@ -26,5 +28,18 @@ public class PlayerNameManager : MonoBehaviour
     {
         PhotonNetwork.NickName = usernameInput.text;
         PlayerPrefs.SetString("username", usernameInput.text);
+        var request = new UpdateUserTitleDisplayNameRequest{
+            DisplayName = usernameInput.text,
+        };
+        PlayFabClientAPI.UpdateUserTitleDisplayName(request, onDisplayNameUpdate, onError);
+    }
+
+    void onDisplayNameUpdate(UpdateUserTitleDisplayNameResult result){
+        Debug.Log("Dispaly name updated");
+    }
+
+    void onError(PlayFabError error){
+        Debug.Log(error.GenerateErrorReport());
+
     }
 }
