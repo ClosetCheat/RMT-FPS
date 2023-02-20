6690b10f9c84b90101e5bc3a636add6b120fac28
SinhroLK <lkrstic001@gmail.com>
2023-02-20 14:11:48 +0100

    Leaderboard

--- FPS/Assets/Scripts/Managers/PlayFabControls.cs
+++ FPS/Assets/Scripts/Managers/PlayFabControls.cs
@@ -10,6 +10,8 @@ public class PlayFabControls : MonoBehaviour
     [SerializeField] GameObject signUpTab, logInTab, startPanel, HUD;
     public TMP_InputField username, userEmail, userPassword, userEmailLogIn, userPasswordLogIn;
     public TMP_Text ErrorSignUp, ErrorLogIn;
+    public GameObject rowPrefab;
+    public Transform rowsParent;
 
     public void SignUpTab(){
         signUpTab.SetActive(true);
@@ -76,6 +78,50 @@ public class PlayFabControls : MonoBehaviour
     void StartGame(){
         startPanel.SetActive(false);
         HUD.SetActive(true);
+    }
+
+    public void SendLeaderboard(int score){
+        var request = new UpdatePlayerStatisticsRequest{
+            Statistics = new List<StatisticUpdate>{
+                new StatisticUpdate{
+                    StatisticName = "Kills",
+                    Value = score
+                }
+            }
+        };
+        PlayFabClientAPI.UpdatePlayerStatistics(request, OnLeaderboardUpdate, onError);
+    }
+
+    public void onError(PlayFabError error){
+        Debug.Log(error.GenerateErrorReport());
+    }
+
+    void OnLeaderboardUpdate(UpdatePlayerStatisticsResult result){
+        Debug.Log("Successful leaderboard update");
+    }
+
+    public void GetLeaderboard(){
+        var request = new GetLeaderboardRequest{
+            StatisticName = "Kills",
+            StartPosition = 0,
+            MaxResultsCount = 5
+        };
+        PlayFabClientAPI.GetLeaderboard(request, OnLeaderboardGet, onError);
+    }
+
+    void OnLeaderboardGet(GetLeaderboardResult result){
+        Debug.Log("Successful Get");
+        // foreach(Transform item in rowsParent){
+        //     Destroy(item.gameObject);
+        // }
+        foreach(var item in result.Leaderboard){
+            GameObject newGo = Instantiate(rowPrefab, rowsParent);
+            TMP_Text[] texts = newGo.GetComponentsInChildren<TMP_Text>();
+            texts[0].text = (item.Position +1).ToString();
+            texts[1].text = item.DisplayName;
+            texts[2].text = item.StatValue.ToString();
+            Debug.Log("Good");
+        }
     }
 }
 
