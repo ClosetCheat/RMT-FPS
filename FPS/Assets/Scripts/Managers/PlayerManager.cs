9acdd521ca8a4511a38c67b214030ea52c9ae218
SinhroLK <lkrstic001@gmail.com>
2023-02-20 11:45:11 +0100

    Kill and Death counter for Playfab

--- FPS/Assets/Scripts/Managers/PlayerManager.cs
+++ FPS/Assets/Scripts/Managers/PlayerManager.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.IO;
 using Photon.Realtime;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
+using PlayFab;
+using PlayFab.ClientModels;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -13,8 +15,8 @@ public class PlayerManager : MonoBehaviour
 
     GameObject controller;
 
-    int kills;
-    int deaths;
+    public int kills;
+    public int deaths;
 
     void Awake()
     {
@@ -33,15 +35,43 @@ public class PlayerManager : MonoBehaviour
         controller = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs","PlayerController"), spawnpoint.position, spawnpoint.rotation, 0, new object[]{PV.ViewID});
     }
 
+    public void onError(PlayFabError error){
+        Debug.Log(error.GenerateErrorReport());
+    }
+
+
     public void Die(){
         PhotonNetwork.Destroy(controller);
         CreateController();
-
+        PlayFabClientAPI.GetUserData(new GetUserDataRequest(), onSuccessDeath, onError);
         deaths++;
 
         Hashtable hash = new Hashtable();
         hash.Add("deaths", deaths);
         PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
+    }
+
+    void onSuccessDeath(GetUserDataResult result){
+        if(result.Data != null && result.Data.ContainsKey("Kills") && result.Data.ContainsKey("Deaths")){
+            string Kills = result.Data["Kills"].Value;
+            string Deaths = result.Data["Deaths"].Value;
+            int numOfDeaths = int.Parse(Deaths);
+            numOfDeaths++;
+            Deaths = numOfDeaths.ToString();
+            PlayFabClientAPI.UpdateUserData(new UpdateUserDataRequest() {
+            Data = new Dictionary<string, string>() {
+                {"Kills", Kills},
+                {"Deaths", Deaths}
+            }},
+            result => Debug.Log("Successfully updated user data"),
+            error => {
+                Debug.Log("Got error setting user data Ancestor to Arthur");
+                Debug.Log(error.GenerateErrorReport());
+            });
+            Debug.Log("Success");
+        } else {
+            Debug.Log("Error");
+        }
     }
 
     public void GetKill()
@@ -52,12 +82,37 @@ public class PlayerManager : MonoBehaviour
     [PunRPC]
     void RPC_GetKill()
     {
+        PlayFabClientAPI.GetUserData(new GetUserDataRequest(), onSuccessKill, onError);
         kills++;
 
         Hashtable hash = new Hashtable();
         hash.Add("kills", kills);
         PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
     }
+
+    void onSuccessKill(GetUserDataResult result){
+        if(result.Data != null && result.Data.ContainsKey("Kills") && result.Data.ContainsKey("Deaths")){
+            string Kills = result.Data["Kills"].Value;
+            string Deaths = result.Data["Deaths"].Value;
+            int numOfKills = int.Parse(Kills);
+            numOfKills++;
+            Kills = numOfKills.ToString();
+            PlayFabClientAPI.UpdateUserData(new UpdateUserDataRequest() {
+            Data = new Dictionary<string, string>() {
+                {"Kills", Kills},
+                {"Deaths", Deaths}
+            }},
+            result => Debug.Log("Successfully updated user data"),
+            error => {
+                Debug.Log("Got error setting user data Ancestor to Arthur");
+                Debug.Log(error.GenerateErrorReport());
+            });
+            Debug.Log("Success");
+        } else {
+            Debug.Log("Error");
+        }
+    }
+
 
     public static PlayerManager Find(Player player)
     {
